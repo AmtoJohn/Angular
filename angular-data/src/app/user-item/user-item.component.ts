707848c7da23e71f3e90817bea3da13dc3be389f
AmtoJohn <amto.john@iisgalvanimi.edu.it>
2022-10-10 12:47:22 +0000

    preparazione punto 1-2

---
+++ angular-data/src/app/user-item/user-item.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-item',
+  templateUrl: './user-item.component.html',
+  styleUrls: ['./user-item.component.css']
+})
+export class UserItemComponent implements OnInit {
+  @Input() name: string; // <-- aggiunta la proprietà name
+  constructor() { 
+
+  }
+  ngOnInit(): void {
+  }
+
+}
